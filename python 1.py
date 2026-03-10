b55eb07c66b7f25a405c682878a9de500f4dee27
514-User <310-Teacher@514-6.spbstu.ru>
2026-03-10 12:24:28 +0300

    Выполнено задание

---
+++ python 1.py
@@ -0,0 +1,18 @@
+student_name="Алипов Андрей Сергеевич"
+group_number="3140801/52501"
+project_name="Жк 3 дырки в сыре"
+floors=21
+height=71.1
+is_residentional=True
+construction_year=2003
+print("=== Паспорт строительного объекта ===")
+print(f"Составитель:{student_name}")
+print(f"Группа:{group_number}")
+print(f"\nОбъект:{project_name}")
+print(f"Этажность:{floors}")
+print(f"Высота:{height}")
+if is_residentional==True:
+    print("Тип: Жилой")
+else:
+    print("Тип: нежилой")
+print(f"Год постройки:{construction_year}")
