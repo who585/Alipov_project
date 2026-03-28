1618babd61ab0436e841356ee615d9d7cc356125
who585 <153118763+who585@users.noreply.github.com>
2026-03-28 23:04:05 +0300

    Задание выполнено

--- task_06_material_catalog.py
+++ task_06_material_catalog.py
@@ -1,1 +1,12 @@
-
+# Каталог материалов
+# Исходные данные
+materials = ["Кирпич", "Бетон", "Штукатурка", "Металл", "Фарфор"]
+print(materials[0])
+print(materials[4])
+print(materials[1:4])
+# Добавление эементов
+materials=materials+['Черепица', 'Стекло']
+# Удаляем элемент
+materials.pop(1)
+print(materials)
+print(len(materials))
