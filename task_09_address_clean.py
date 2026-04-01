7f574bf6efb8a3dec487ffdc9ddcb8a991709bcf
who585 <153118763+who585@users.noreply.github.com>
2026-04-02 00:49:44 +0300

    Задание выполнено

--- task_09_address_clean.py
+++ task_09_address_clean.py
@@ -1,1 +1,16 @@
-
+# Очистка адресов
+# Исходные данные
+addresses = [ 
+"  г. Москва, ул. Ленина, д. 10  ", 
+"г.Казань,ул.Баумана,д.15", 
+"  г. Санкт-Петербург, ул. Невский, д. 100  " 
+]
+# Создаем пустой список
+cleaned_addresses = []
+# Прогоняем каждый адрес
+for address in addresses:
+  address_1=address.strip().replace(" ","").replace(".",". ").replace(",",", ")
+  cleaned_addresses.append(address_1) # Добавляем в пустой список адреса
+print(f'#1\nДо: {addresses[0]} \nПосле: {cleaned_addresses[0]}')
+print(f'#2\nДо: {addresses[1]} \nПосле: {cleaned_addresses[1]}')
+print(f'#3\nДо: {addresses[2]} \nПосле: {cleaned_addresses[2]}')
