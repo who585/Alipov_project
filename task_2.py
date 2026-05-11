d6bc771ea477e1fff3f78bfe1f717cbf21e5babb
who585 <153118763+who585@users.noreply.github.com>
2026-05-11 22:39:58 +0300

    Update task_2.py

--- task_2.py
+++ task_2.py
@@ -1,13 +1,7 @@
 df = pd.read_csv('5 train.csv')
 
-hot = df[df['count'] > 500]
+seasonal = df.groupby('season')[['casual', 'registered']].mean()
 
-seasons = hot.groupby('season').size()
-
-top_season = seasons.idxmax()
-top_count = seasons.max()
-
-print(f'Всего наблюдений  > 500: {len(hot)}')
-print(f' СЕЗОН: {top_season} ')
-print(f'поездок: {top_count}')
-
+seasonal.plot(kind='bar')
+plt.xlabel("Время года")
+plt.ylabel("Среднее число поездок")
