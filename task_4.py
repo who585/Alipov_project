fded0406a6dd2286d8688a01246aa61a4ed28982
who585 <153118763+who585@users.noreply.github.com>
2026-05-11 22:40:22 +0300

    Update task_4.py

--- task_4.py
+++ task_4.py
@@ -3,4 +3,3 @@ df['datetime'] = pd.to_datetime(df['datetime'])
 hourly_rides = df.groupby(weekend_rides['datetime'].dt.month)[['temp','count']].mean()
 hourly_rides.to_csv("Средняя температура и число поездок по месяцам.csv", sep=";")
 
-
