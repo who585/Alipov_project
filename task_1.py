1022b3f2a65e83fd19df34900436f4575b223309
who585 <153118763+who585@users.noreply.github.com>
2026-05-11 22:39:44 +0300

    Update task_1.py

--- task_1.py
+++ task_1.py
@@ -1,11 +1,13 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-
 df = pd.read_csv('5 train.csv')
+df['datetime'] = pd.to_datetime(df['datetime'])
+weekend_mask = df['datetime'].dt.dayofweek >= 5
+weekend_rides = df[weekend_mask]
+hourly_rides = weekend_rides.groupby(weekend_rides['datetime'].dt.hour)['count'].sum()
 
-seasonal = df.groupby('season')[['casual', 'registered']].mean()
+max_hour = hourly_rides.idxmax()
+max_rides = hourly_rides.max()
 
-seasonal.plot(kind='bar')
-plt.xlabel("Время года")
-plt.ylabel("Среднее число поездок")
-
+print(f" В выходные дни час пик — это {max_hour} часов.")
+print(f" В это время совершается {max_rides} поездок")
